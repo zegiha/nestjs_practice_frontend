22fae2af69a6c0119c365a88b525fe55c9c10c93
zegiha <LeeSeoYul070814@gmail.com>
2024-05-03 07:03:10 +0900

    commit message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,37 @@
-import {Fragment} from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import SimpleNotice from "../components/SimpleNotice";
 
 const contents = "내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용"
 
+interface Data {
+	message: string,
+}
+
 const Home = () => {
+	const [data, setData] = useState<Data | null>(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await fetch("http://localhost:8000/");
+				const tmp = await res.json();
+				setData(tmp);
+				console.log(tmp);
+			}
+			catch(error) {
+				console.log(error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
 	return (
 		<Fragment>
 			<SimpleNotice title={"제목"} contents={contents} author={"글쓴이"} date={"2024-04-14"}/>
+			{data && (
+				<h1>{data.message}</h1>
+			)}
 		</Fragment>
 	);
 };
